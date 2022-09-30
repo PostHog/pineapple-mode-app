0ca2f975c2baa2c6092738d92f3b8c6b24d5169f
Marius Andra <marius.andra@gmail.com>
2022-09-30 11:56:43 +0200

    prettier

--- web.ts
+++ web.ts
@@ -1,28 +1,28 @@
-function makeItRain(emoji: string) {
-  const div = document.createElement("div");
-  Object.assign(div.style, {
-    position: "fixed",
-    left: `${(window.innerWidth - 30) * Math.random()}px`,
-    top: "-10px",
-    fontSize: "24px",
-    zIndex: 99999999,
-    pointerEvents: "none",
-  });
-  div.innerHTML = emoji;
-  window.document.body.appendChild(div);
-  const duration = 1000 + Math.random() * 3000;
-  div.animate([{ top: "-10px" }, { top: `${window.innerHeight + 20}px` }], {
-    duration,
-    iterations: 1,
-  });
-  window.setTimeout(() => div.remove(), duration + 1);
+export function inject({ config, posthog }) {
+    const emoji = config.emoji || '🍍'
+    for (let i = 0; i < 10; i++) {
+        makeItRain(emoji)
+    }
+    window.setInterval(() => makeItRain(emoji), 200)
+    posthog.capture('made it rain', { emoji })
 }
 
-export function inject({ config, posthog }) {
-  const emoji = config.emoji || "🍍";
-  for (let i = 0; i < 10; i++) {
-    makeItRain(emoji);
-  }
-  window.setInterval(() => makeItRain(emoji), 200);
-  posthog.capture("made it rain", { emoji });
+function makeItRain(emoji: string) {
+    const div = document.createElement('div')
+    Object.assign(div.style, {
+        position: 'fixed',
+        left: `${(window.innerWidth - 30) * Math.random()}px`,
+        top: '-10px',
+        fontSize: '24px',
+        zIndex: 99999999,
+        pointerEvents: 'none',
+    })
+    div.innerHTML = emoji
+    window.document.body.appendChild(div)
+    const duration = 1000 + Math.random() * 3000
+    div.animate([{ top: '-10px' }, { top: `${window.innerHeight + 20}px` }], {
+        duration,
+        iterations: 1,
+    })
+    window.setTimeout(() => div.remove(), duration + 1)
 }
